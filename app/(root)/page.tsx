eaba5be86bd39af07a858c7e6137b77da8b5b25c
Davide Filosa <davideclaypool@gmail.com!>
2023-07-11 10:21:16 +0200

    create modal component and  provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="p-4">
-      <p>Hello admin dashboard</p>
-      <Button size="default" variant="destructive">
-        Click Me
-      </Button>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className="p-4">Root page</div>;
 }
