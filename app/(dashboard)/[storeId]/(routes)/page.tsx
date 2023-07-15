bc9dd2709c9fade3abd8f6d06ffc5ea60435634b
Davide Filosa <davideclaypool@gmail.com!>
2023-07-15 08:46:09 +0200

    created patch and delete route

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,10 +1,6 @@
 import prismadb from "@/lib/prismadb";
 
-interface DashboardPageProps {
-  params: { storeId: string };
-}
-
-const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+const DashboardPage = async ({ params }: { params: { storeId: string } }) => {
   const store = await prismadb.store.findFirst({
     where: { id: params.storeId },
   });
