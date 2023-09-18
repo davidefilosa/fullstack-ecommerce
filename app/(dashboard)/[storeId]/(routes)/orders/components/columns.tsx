24d2ea8aea3a89c012e6f72bb59f572828bdb7bb
Davide Filosa <davideclaypool@gmail.com!>
2023-09-18 12:02:45 +0200

    created order page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  products: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "addrress",
+    header: "Address",
+  },
+  {
+    accessorKey: "products",
+    header: "Products",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+];
---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/order-client.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { DataTable } from "@/components/ui/data-table";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { OrderColumn, columns } from "./columns";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="See orders for your store"
+      />
+
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="products" />
+    </>
+  );
+};
+
+export default OrderClient;
