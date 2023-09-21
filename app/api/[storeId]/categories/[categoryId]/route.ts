481449f1de3c2fefe4d93cfe4d9d416d30eed1cb
Davide Filosa <davideclaypool@gmail.com!>
2023-09-21 11:07:11 +0200

    modified API to include Billborad in Category

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -98,6 +98,7 @@ export async function GET(
 
     const category = await prismadb.category.findUnique({
       where: { id: params.categoryId },
+      include: { billboard: true },
     });
     return NextResponse.json(category);
   } catch (error) {
